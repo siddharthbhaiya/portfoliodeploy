af2ddea58a9abcc0b1d64c7f25bfa6f60870276b
Sidsingh2010 <siddharthsekhar2010@gmail.com>
2025-05-04 20:02:56 +0530

    modified header and added download functionality for resume

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,14 +4,17 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="magic-header">
-<div className="glow-title">🚀 &lt;StackOverSiddharth/&gt;</div>
-<nav className="nav-items">
+      <div className="glow-title">🚀 &lt;StackOverSiddharth/&gt;</div>
+      <nav className="nav-items">
         <a href="#intro">👨‍💻 About</a>
         <a href="#skills">🧠 Skills</a>
         <a href="#projects">💼 Projects</a>
         <a href="#experience">📈 Experience</a>
         <a href="#contact">📬 Contact</a>
-        <a href="#resume">📄 Resume</a>
+
+        <a href="/Siddharth_front_end_developer.pdf" download>
+          📄 Download Resume
+        </a>
       </nav>
     </header>
   );
